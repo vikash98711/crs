e64d01e57c194c94164822c8b344d9471510fbd6
vikash987113 <vikashrstylo123456@gmail.com>
2023-11-18 11:52:39 +0530

    changes

--- config.js
+++ config.js
@@ -1,18 +1,6 @@
 const sql = require('mssql');
 require('dotenv').config();
 
-
-// const config = {
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   server: process.env.DB_SERVER,
-//   database: process.env.DB_DATABASE,
-//   options: {
-//     encrypt: true,
-//     trustServerCertificate: true,
-//   },
-// };
-
 const connectionString = process.env.DATABASE_URL || `mssql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_SERVER}/${process.env.DB_DATABASE}`;
 
 const config = {
@@ -23,16 +11,12 @@ const config = {
   },
 };
 
-
-
-
-
 const checkDbConnection = async () => {
   try {
     const pool = await sql.connect(config);
     if (pool.connected) {
       console.log('Database connection successful');
-      return pool; 
+      return pool;
     } else {
       console.log('Database connection failed');
     }
